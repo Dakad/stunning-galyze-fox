83e28bec4c5b28a4728126eeca331cbd98b42ce6
Dakad <3106338+Dakad@users.noreply.github.com>
2019-05-29 00:59:06 +0200

    Process comic dirs to create new cmoci filenames

--- rename-comix-folder.py
+++ rename-comix-folder.py
@@ -25,31 +25,72 @@ def run(comic_dir_path, saving_dir_path):
         return os.path.isdir(os.path.join(comic_dir_path, dir_name))
 
     dirs = sorted(filter(join_with_comic_dir_func, os.listdir(comic_dir_path)))
-
+    comics = {}
     for comic_dir in dirs:
-        new_comic_name, num_serie = parse_comic_name(comic_dir)
-        if num_serie != None:
-            print(new_comic_name, int(num_serie))
-        # new_comics_names = scan_comic_dir(comic_dir_path)
-        pass
+        comic_name, num_serie = parse_comic_name(comic_dir)
+
+        # Little eception for an custom folder victim of I/O Error
+        if(num_serie != None and num_serie == 99):
+            continue
+
+        # Retrieve the comic
+        file_names = comics.get(comic_name, [])
+
+        # Rename this comic files
+        dir_path = os.path.join(comic_dir_path, comic_dir)
+
+        new_comics_names = process_comic_dir(dir_path, num_serie)
+        # print(comics, file_names, new_comics_names)
+        # Add the renamed comic filenames
+        comics[comic_name] = file_names + new_comics_names
+
         # TODO Rename the new list
 
 
 def parse_comic_name(comic_name):
+    """Retrieve the comic nam and the serie number
+
+    Arguments:
+        comic_name {String} -- The comic name
+
+    Returns:
+
+        [String] -- Comic title with the author
+
+        [Number] -- Comic serie number
+    """
+
     match = pattern.match(comic_name)
     if match:
         group = match.groupdict()
-        return (group['title'], group['num_serie'])
+        return (group['title'], int(group['num_serie']))
     else:
         return (comic_name, None)
 
 
-def scan_comic_dir(comic_dir_path, num_serie=None):
+def process_comic_dir(comic_dir_path, num_serie=None):
+    """Scan and rename the comic file
+
+    Arguments:
+        comic_dir_path {[type]} -- [description]
+
+    Keyword Arguments:
+        num_serie {[type]} -- [description] (default: {None})
+    """
+    new_comic_names = []
     with os.scandir(comic_dir_path) as comic_dir:
-        for entry in sorted(comic_dir, key=lambda e: e.name):
-            if entry.is_dir():
-                # TODO PArse the dir name to retrieve the num
-                scan_comic_dir(comic_dir)
+        for i, entry in enumerate(sorted(comic_dir, key=lambda e: e.name)):
+            if not entry.is_file():
+                return
+
+            ext = os.path.splitext(entry.name)[1]
+            new_name = '{:0>3}'.format(i+1) + ext
+            if num_serie != None:
+                new_name = '{}_'.format(num_serie) + new_name
+            names = (entry.path, new_name)
+            new_comic_names.append(names)
+
+    return new_comic_names
 
 
 if __name__ == "__main__":
