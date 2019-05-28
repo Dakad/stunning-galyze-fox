9231486d6a280533754f3da3d762d4f047202f1c
Dakad <3106338+Dakad@users.noreply.github.com>
2019-05-28 22:39:20 +0200

    Check the sys args

--- rename-comix-folder.py
+++ rename-comix-folder.py
@@ -4,8 +4,14 @@ import sys
 
 def run(comic_dir_path, saving_dir_path):
     # TODO Check if the comic and saving dir exists
+    if not os.path.isdir(comic_dir_path):
+        print("The provided comic dir '%s' do not exist" %
+              comic_dir_path, flush=True)
+        sys.exit(1)
 
-    # TODO Create the saving dir if not exists
+    if not os.path.isdir(saving_dir_path):
+        print("Creating saving dir : ", saving_dir_path, flush=True)
+        os.makedirs(saving_dir_path, exist_ok=True)
 
     # TODO Scan the comic dir
     pass
@@ -13,19 +19,22 @@ def run(comic_dir_path, saving_dir_path):
 
 if __name__ == "__main__":
     message = """
-        This script will rename the comic folders with  the correct format.
-        Renamed on a range of number starting by 001, 002, ....
-        (depending of the folder name).
-        If the folder is part of a serie, the serie number is added to the new filename
-        Syntax : python {0} <comic_directory> <saving_directory>
-            where <comic_directory> is the comic directory
-                  <saving_directory> where to save the renamed files
-                    Will be created if not exist.
-        Examples : 
-            python {0} ../Comics/  ~/Comics/Organized/
+This script will rename the comic folders with  the correct format.
+Renamed on a range of number starting by 001, 002, ....
+(depending of the folder name).
+
+If the folder is part of a serie, the serie number is added to the new filename
+    """
+
+    syntax = """
+Syntax : python {0} <comic_directory> <saving_directory>
+            <comic_directory> is the comic directory
+            <saving_directory> where to save the renamed files. Will be created if not exist.
+Example : python {0} ../Comics/  ~/Comics/Organized/
     """.format(sys.argv[0])
 
     if len(sys.argv) != 3:
-        print(message)
+        print("Bad usage of ", sys.argv[0])
+        print(syntax)
     else:
         run(*sys.argv[1:])
